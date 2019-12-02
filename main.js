70abe5ceb471ee8a81405c04b2805d9adde73ac3
Alessandro_Mondelli <alexs98@hotmail.it>
2019-12-02 23:03:54 +0100

    Fine stile statito, aggiunto bonus(Cambio icona quando l'utente vuole scrivere un messaggio)

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#write-mess").click(function() {
+        $(".fa-microphone").removeClass("fa-microphone");
+        $("#send-button i").addClass("fa-paper-plane");
+    })
+})
