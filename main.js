413004b7e1bb93a612e4d558975507e9369659c1
Alessandro_Mondelli <alexs98@hotmail.it>
2019-12-03 18:30:17 +0100

    Aggiunta ricerca dei nomi in tempo reale(Per ogni lettera scritta si aggiorna la lista dei contatti in base alle corrispondenze)

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+                    //*** INVIO DEI MESSAGGI ***//
     $("#send-button").click(function() { //Al click su invio messaggio
         getMessSendMess(); //Richiamo funzione per inviare messaggio
     });
@@ -19,43 +19,65 @@ $(document).ready(function() {
         }
     });
 
+                   //***RICERCA UTENTE CHAT ***//
+    $( ".search-zone input").keyup(function() {
+        var getUser = $(".search-zone input").val(); //Prendo in una variabile la ricerca effettuata
+        getUser = getUser.toLowerCase(); //Riduco le lettere scritte dall'utente a minuscole
 
-    function getMessSendMess() { //Funzione per inviare messaggi
-        var messageText = $(".write-zone input").val(); //Prendo in una variabile il messaggio scritto dall'utente
-
-        if (messageText.length != 0) {
-            var newText = $(".template .mess-t").clone(); //Clono template presente in html
-            var time = setHours();
-            newText.children(".mess").text(messageText); //Assegno al figlio "mess" il messaggio preso in input
-            newText.children(".mess-time").text(time);
-
-            newText.addClass("sent"); //Gli aggiungo la classe sent
-            $(".central-message.active").append(newText); //Lo appendo nella classe dei messaggi
-            $(".write-zone input").val(""); //Azzero l'input ogni volta che il messaggio viene inviato
-
-            receivedMess(); //Richiamo funzione che riceve messaggio
-        }
-    }
-
-    function receivedMess() {
-        var messageReceived = "Ok!";
-
-        setTimeout(function() {
-            var newText = $(".template .mess-t").clone(); //Clono template presente in html
-            var time = setHours();
-            newText.children(".mess").text(messageReceived); //Assegno al figlio "mess" il messaggio preso in input
-            newText.children(".mess-time").text(time);
-
-            newText.addClass("received"); //Gli aggiungo la classe sent
-            $(".central-message.active").append(newText); //Lo appendo nella classe dei messaggi
-        },1000);
-    }
-
-    function setHours() { //Funzione che recura ora attuale
-        var date = new Date; //Variabile che tiene tutte le informazione sulla data
-
-        var minutes = date.getMinutes(); //Prendo i minuti
-        var hour = date.getHours(); //Prendo le ore
-        return (hour + ":"+ minutes); //Invio ore e minti
-    }
+        var getContacts = []; //Array dove verranno inseriti i nomi dei contatti
+        var contacts = $(".mess-conv p"); //Contatti presenti
+        contacts.each(function(cont) { //Scorro uno ad uno i contatti
+            var temp = $(this).text(); //Prendo i nomi dei contatti
+            temp = temp.toLowerCase(); //Metto tutti i contatti in minuscolo
+            // getContacts.push(temp); // metto i contatti in un array
+            if ((temp.includes(getUser)) && (getUser.length != 0)) {
+                getContacts.push(temp);
+                $(this).parentsUntil("#conversations").show();
+            } else if (getUser.length == 0){
+                $(this).parentsUntil("#conversations").show();
+            } else {
+                $(this).parentsUntil("#conversations").hide();
+            }
+        })
+    });
 });
+
+                    //*** FUNZIONI ***//
+function getMessSendMess() { //Funzione per inviare messaggi
+    var messageText = $(".write-zone input").val(); //Prendo in una variabile il messaggio scritto dall'utente
+
+    if (messageText.length != 0) {
+        var newText = $(".template .mess-t").clone(); //Clono template presente in html
+        var time = setHours();
+        newText.children(".mess").text(messageText); //Assegno al figlio "mess" il messaggio preso in input
+        newText.children(".mess-time").text(time);
+
+        newText.addClass("sent"); //Gli aggiungo la classe sent
+        $(".central-message.active").append(newText); //Lo appendo nella classe dei messaggi
+        $(".write-zone input").val(""); //Azzero l'input ogni volta che il messaggio viene inviato
+
+        receivedMess(); //Richiamo funzione che riceve messaggio
+    }
+}
+
+function receivedMess() {
+var messageReceived = "Ok!";
+
+setTimeout(function() {
+    var newText = $(".template .mess-t").clone(); //Clono template presente in html
+    var time = setHours();
+    newText.children(".mess").text(messageReceived); //Assegno al figlio "mess" il messaggio preso in input
+    newText.children(".mess-time").text(time);
+
+    newText.addClass("received"); //Gli aggiungo la classe sent
+    $(".central-message.active").append(newText); //Lo appendo nella classe dei messaggi
+    },1000);
+}
+
+function setHours() { //Funzione che recura ora attuale
+    var date = new Date; //Variabile che tiene tutte le informazione sulla data
+
+    var minutes = date.getMinutes(); //Prendo i minuti
+    var hour = date.getHours(); //Prendo le ore
+    return (hour + ":"+ minutes); //Invio ore e minti
+}
