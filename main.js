e274a28d7314308b243da639ccff528d25e28ac3
Alessandro_Mondelli <alexs98@hotmail.it>
2019-12-04 18:22:18 +0100

    Aggiunta possibilità di cambiare conversazioni e scrivere messaggi ad ogni contatto

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
         'imgCont' : "https://image.flaticon.com/icons/svg/145/145862.svg",
         'name' : ' Alessia ',
         'lastMess' : 'Ciao! Come stai?',
+        'lastAccess': 'Ultimo accesso oggi alle 10:40',
         'chatCode' : 'chat1'
     }
 
@@ -13,6 +14,7 @@ $(document).ready(function() {
         "imgCont" : "https://image.flaticon.com/icons/svg/145/145859.svg",
         "name" : "Mario",
         "lastMess" : "Io sto bene",
+        'lastAccess': 'Ultimo accesso oggi alle 12:14',
         "chatCode" : "chat2"
     }
 
@@ -20,6 +22,7 @@ $(document).ready(function() {
         "imgCont" : "https://image.flaticon.com/icons/svg/145/145852.svg",
         "name" : "Francesca",
         "lastMess" : "Che fai?",
+        'lastAccess': ' Ultimo accesso oggi alle 13:15',
         "chatCode" : "chat3"
     }
 
@@ -27,20 +30,22 @@ $(document).ready(function() {
         "imgCont" : "https://image.flaticon.com/icons/svg/145/145864.svg",
         "name" : "Maria",
         "lastMess" : "Che fai?",
+        'lastAccess': ' Ultimo accesso oggi alle 07:14',
         "chatCode" : "chat4"
     }
 
     var elencoCont = [contattoAlessia,contattoMario,contattoFrancesca,contattoMaria]; //Array oggetti
-    console.log(elencoCont);
 
-//*** CREO LISTA CONTATTI ***//
+                    //*** CREO LISTA CONTATTI ***//
     for (var i = 0; i < elencoCont.length; i++) { //Scorro ogni elemento dell'array
         var cont_att = elencoCont[i]; //Prendo dati di ogni singolo oggetto
         var imgContAtt = cont_att.imgCont; //Dati immagine
         var nameAtt = cont_att.name; //Dati nome
         var lastMessAtt = cont_att.lastMess; //Dati ultimo messaggio
+        var lastAccessAtt = cont_att.lastAccess; //Dati ultimo messaggio
         var chatCodeAtt = cont_att.chatCode; //dati codice chat
 
+        //Creo i contatti su HTML
         $("#conversations").append(`
             <div class="n-conv" data-chatCode="` + chatCodeAtt + `">
                 <div class="img-conv">
@@ -51,7 +56,48 @@ $(document).ready(function() {
                     <span>` + lastMessAtt + `<span>
                 </div>
             `);
+
+        //Creo immagini superiori
+        $(".top-images.change").append(`
+            <div class="top-img-pos" data-chatCode="` + chatCodeAtt + `">
+                <img src="` + imgContAtt + `" alt="Immagine Contatto">
+                <p>` + nameAtt + `</p>
+                <p>` + lastAccessAtt + `</p>
+            </div>
+        `)
+
+        //Creo zone messaggi
+        $(".central-message-zone").append(`
+            <div class="central-message" data-chatCode="` + chatCodeAtt + `"></div>
+        `)
     }
+
+                    //*** SCELTA CHAT ***/
+    $("#conversations .n-conv").click(function() {
+        var temp = (this).dataset.chatcode; //Ricevo dal click quale contatto è stato cliccato
+        switch (temp) { //In base al contatto cliccato mostro la chat
+            case "chat1": //In caso di chat 1
+                addRemoveClass($(".n-conv[data-chatcode ='chat1']"),$(".n-conv")); //Aggiungo colore di sfondo al contatto selezionato
+                addRemoveClass($(".top-img-pos[data-chatcode ='chat1']"),$(".top-img-pos")); //Aggiungo foto del contatto nella chat
+                hideShow($(".central-message"),$(".central-message[data-chatcode ='chat1']")); //Mostro la conversazione richiesta
+            break;
+            case "chat2": //In caso di chat 2
+                addRemoveClass($(".n-conv[data-chatcode ='chat2']"),$(".n-conv")); //Aggiungo colore di sfondo al contatto selezionato
+                addRemoveClass($(".top-img-pos[data-chatcode ='chat2']"),$(".top-img-pos")); //Aggiungo foto del contatto nella chat
+                hideShow($(".central-message"),$(".central-message[data-chatcode ='chat2']")); //Mostro la conversazione richiesta
+            break;
+            case "chat3": //In caso di chat 3
+                addRemoveClass($(".n-conv[data-chatcode ='chat3']"),$(".n-conv"),); //Aggiungo colore di sfondo al contatto selezionato
+                addRemoveClass($(".top-img-pos[data-chatcode ='chat3']"),$(".top-img-pos")); //Aggiungo foto del contatto nella chat
+                hideShow($(".central-message"),$(".central-message[data-chatcode ='chat3']")); //Mostro la conversazione richiesta
+            break;
+            case "chat4": //In caso di chat 4
+                addRemoveClass($(".n-conv[data-chatcode ='chat4']"),$(".n-conv")); //Aggiungo colore di sfondo al contatto selezionato
+                addRemoveClass($(".top-img-pos[data-chatcode ='chat4']"),$(".top-img-pos")); //Aggiungo foto del contatto nella chat
+                hideShow($(".central-message"),$(".central-message[data-chatcode ='chat4']")); //Mostro la conversazione richiesta
+            break;
+        }
+    });
 
 
                     //*** INVIO DEI MESSAGGI ***//
@@ -99,14 +145,14 @@ $(document).ready(function() {
 function getMessSendMess() { //Funzione per inviare messaggi
     var messageText = $(".write-zone input").val(); //Prendo in una variabile il messaggio scritto dall'utente
 
-    if (messageText.length != 0) {
+    if (messageText.length != 0 && $(".n-conv").hasClass("active")) {
         var newText = $(".template .mess-t").clone(); //Clono template presente in html
         var time = setHours();
         newText.children(".mess").text(messageText); //Assegno al figlio "mess" il messaggio preso in input
         newText.children(".mess-time").text(time);
 
         newText.addClass("sent"); //Gli aggiungo la classe sent
-        $(".central-message.active").append(newText); //Lo appendo nella classe dei messaggi
+        $(".central-message:visible").append(newText); //Lo appendo nella classe dei messaggi
         $(".write-zone input").val(""); //Azzero l'input ogni volta che il messaggio viene inviato
 
         receivedMess(); //Richiamo funzione che riceve messaggio
@@ -123,7 +169,7 @@ setTimeout(function() {
     newText.children(".mess-time").text(time);
 
     newText.addClass("received"); //Gli aggiungo la classe sent
-    $(".central-message.active").append(newText); //Lo appendo nella classe dei messaggi
+    $(".central-message:visible").append(newText); //Lo appendo nella classe dei messaggi
     },1000);
 }
 
@@ -133,4 +179,14 @@ function setHours() { //Funzione che recura ora attuale
     var minutes = date.getMinutes(); //Prendo i minuti
     var hour = date.getHours(); //Prendo le ore
     return (hour + ":"+ minutes); //Invio ore e minti
+}
+
+function addRemoveClass(add,remove) {
+    remove.removeClass("active");
+    add.addClass("active");
+}
+
+function hideShow(hideEl,showEl) {
+    hideEl.hide();
+    showEl.show();
 }
